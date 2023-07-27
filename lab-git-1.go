45701b1140637220e83e5e601c02092b70e1cf25
Anvar Nabij <nabijonov9524@gmail.com>
2023-07-27 12:04:27 +0500

    add summ func

--- lab-git-1.go
+++ lab-git-1.go
@@ -1,6 +1,20 @@
 package main
 import "fmt"
-func main(){
-fmt.Println("fff")
 
+func sumInt(ar ...int) (int, int) {
+    var i int
+    i = 0
+    var sum int
+    sum = 0
+    for _, elem := range ar{
+        sum += elem
+        i++
+    }
+    return i, sum
+}
+
+func main(){
+	fmt.Println("fff")
+	a,b := sumInt(5,6,8)
+	fmt.Println(a,b)
 }
